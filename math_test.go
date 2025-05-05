860e6c675d0f581385867aee448b53b806e2acf0
barba-rossa <barbosaa.felipe@gmail.com>
2025-05-05 09:16:32 -0600

    initial commit, practicing github actions

---
+++ math_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := sum(15, 15)
+
+	if total != 30 {
+		t.Errorf("Invalid sum result: got %d, expected %d", total, 30)
+	}
+}
